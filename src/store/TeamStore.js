8bb5a6ab3f6b44bf2a820240002d521b04001705
Murhussain <husseinmurashi@yahoo.com>
2023-03-27 15:05:26 +0200

    Adding a team store with all necessary properties

--- src/store/TeamStore.js
+++ src/store/TeamStore.js
@@ -1,19 +1,27 @@
 import { defineStore } from "pinia";
 
 export let useTeamStore = defineStore('team', {
-  state() {
-    return {
-      name: '',
-      spots: 0,
-      members:[]
-    }
-  },
+  state: () => ({
+    name: '',
+    spots: 0,
+    members: []
+  }),
 
   actions: {
     async fill() {
       let r = await import('@/team.json');
 
-        this.$state = r.default;
+      this.$state = r.default;
+    },
+
+    grow(spots) {
+      this.spots = spots;
+    }
+  },
+
+  getters: {
+    spotsRemaining() {
+      return this.spots - this.members.length;
     }
   }
 });
