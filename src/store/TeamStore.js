e360b1b169c663bea56ef278813fd5bec7096afd
Murhussain <husseinmurashi@yahoo.com>
2023-03-27 14:46:04 +0200

    The first way to use pinia to read data

---
+++ src/store/TeamStore.js
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia";
+
+export let useTeamStore = defineStore('team', {
+  state() {
+    return {
+      name: '',
+      spots: 0,
+      members:[]
+    }
+  },
+
+  actions: {
+    fill() {
+      import('@team.json').then(r => {
+        let data = r.default;
+
+        this.name = data.name;
+        this.spots = data.spots;
+        this.members = data.members;
+      });
+    }
+  }
+});
